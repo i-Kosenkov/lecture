41a37d311a6139cf7ea32537dc504c3226f260ef
Kosenkov <9588434@gmail.com>
2022-08-18 22:06:49 +0300

    Change Lecture 003

--- Lecture003/Program.cs
+++ Lecture003/Program.cs
@@ -1,10 +1,4 @@
 ﻿Console.Write("введите день недели 3 или 5: ");
 int x = Convert.ToInt32(Console.ReadLine());
-if (x == 3)
-{
-    Console.WriteLine("Среда");
-}
-if (x == 5)
-{
-    Console.WriteLine("Пятница");
-}
+if (x == 3) Console.WriteLine("Среда");
+if (x == 5) Console.WriteLine("Пятница");
