e96de37bc0b4074af0213eecf774e34a32bf15c8
Kosenkov <9588434@gmail.com>
2022-08-18 20:56:57 +0300

    Modified Lecture 002

---
+++ Lecture002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое значение: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе значение: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (y == x / y)
+{
+    Console.WriteLine("Первое значение является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое значение не является квадратом второго");
+}
